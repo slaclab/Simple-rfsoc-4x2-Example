658594c2e8f5661789c07be06bf92c7ccdd4a0a3
Larry Ruckman <ruckman@slac.stanford.edu>
2025-04-15 15:25:39 -0700

    updating ui path method

--- software/scripts/devGui.py
+++ software/scripts/devGui.py
@@ -32,8 +32,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--ip",
         type     = str,
-        required = False,
-        default  = '10.0.0.10',
+        required = True,
         help     = "ETH Host Name (or IP address)",
     )
 
@@ -65,9 +64,6 @@ if __name__ == "__main__":
     # Get the arguments
     args = parser.parse_args()
 
-    top_level = os.path.realpath(__file__).split('software')[0]
-    ui = top_level+'firmware/submodules/axi-soc-ultra-plus-core/python/axi_soc_ultra_plus_core/rfsoc_utility/gui/GuiTop.py'
-
     #################################################################
 
     with simple_rfsoc_4x2_example.Root(
@@ -78,7 +74,7 @@ if __name__ == "__main__":
     ) as root:
         axi_soc_ultra_plus_core.rfsoc_utility.pydm.runPyDM(
             serverList = root.zmqServer.address,
-            ui       = ui,
+            ui       = f'{os.path.dirname(axi_soc_ultra_plus_core.rfsoc_utility.__file__)}/gui/GuiTop.py',
             sizeX    = 800,
             sizeY    = 800,
             numAdcCh = 4,
